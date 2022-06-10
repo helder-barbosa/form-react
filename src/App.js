16050c3c20b1a6047ded9aff3e454bbfb54145ec
Helder Barbosa <helderbarbosa.dev@gmail.com>
2022-06-09 22:57:37 -0300

    #1 First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import FormInput from './components/FormInput';
 
 function App() {
+  const [values, setValues] = useState({
+    username: '',
+    email: '',
+    birthday: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: 'username',
+      type: 'text',
+      placeholder: 'Username',
+      errorMessage:
+        "Username should be 3-16 characters and shouldn't include any special character !",
+      label: 'Username',
+      pattern: '^[A-Za-z0-9]{3,16}$',
+      required: true,
+    },
+    {
+      id: 2,
+      name: 'email',
+      type: 'email',
+      placeholder: 'Email',
+      errorMessage: 'It Should be a valid email !',
+      label: 'Email',
+      required: true,
+    },
+    {
+      id: 3,
+      name: 'birthday',
+      type: 'date',
+      placeholder: 'Birthday',
+      label: 'Birthday',
+    },
+    {
+      id: 4,
+      name: 'password',
+      type: 'password',
+      placeholder: 'Password',
+      errorMessage:
+        'Password should have 8-20 characters and include at least 1 letter, 1 number and 1 special character !',
+      label: 'Password',
+      pattern:
+        '^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$',
+      required: true,
+    },
+    {
+      id: 5,
+      name: 'confirmPassword',
+      type: 'password',
+      placeholder: 'Confirm Password',
+      errorMessage: "Password don't match !",
+      label: 'Confirm Password',
+      pattern: values.password,
+      required: true,
+    },
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
+
+  const onChange = (evt) => {
+    setValues({ ...values, [evt.target.name]: evt.target.value });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+        <h1>Register</h1>
+        {inputs.map((input) => {
+          return (
+            <FormInput
+              key={input.id}
+              {...input}
+              value={values[input.name]}
+              onChange={onChange}
+            />
+          );
+        })}
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
